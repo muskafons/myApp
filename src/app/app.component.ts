5356d535101f67d9126f89bd5154646e4f285283
docgestion.dtic <docgestion.dtic@uptc.edu.co>
2022-03-22 18:16:34 -0500

    primer registro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.modelo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myApp';
+  empleados: Empleado[] = [
+    new Empleado('Julian', 'Fonseca', 'tecnico', 1000),
+    new Empleado('Camilo', 'Fonseca', 'tecnico', 1500),
+    new Empleado('Jaen', 'Rutts', 'auxiliar', 900),
+    new Empleado('Marian', 'Reds', 'ingeniera', 1200),
+  ];
+  titulo = 'REGISTRO DE USUARIOS';
 }
