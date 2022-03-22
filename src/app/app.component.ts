ba0db1a5a229cdf23b423156f3c8a271af4a6b86
docgestion.dtic <docgestion.dtic@uptc.edu.co>
2022-03-22 11:21:34 -0500

    validacion basica Usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myApp';
+  nombre: string = '';
+  apellido: string = '';
+  mensaje: string = '';
+
+  registrado: boolean = false;
+
+  color: string = '';
+  colorFont: string = '';
+
+  titulo = 'REGISTRO DE USUARIO';
+
+  registrar(): void {
+    if (this.nombre == '' || this.apellido == '') {
+      this.registrado = true;
+      this.color = 'bg-danger bg-gradient';
+      this.colorFont = 'color: aliceblue';
+      this.mensaje = 'Digite nombre y apellido !!!';
+    } else {
+      this.registrado = true;
+      this.color = 'bg-success bg-gradient';
+      this.colorFont = 'color: aliceblue';
+      this.mensaje =
+        this.nombre + ' ' + this.apellido + ' registrado con exito';
+    }
+  }
 }
