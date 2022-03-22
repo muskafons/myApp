126d23642f902b622556ea023163a27c93bc2d2a
muskafons <muskafons@hotmail.com>
2022-03-21 21:03:45 -0500

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myApp';
+  title = 'Caculadora';
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado:number = 0;
+
+  sumar():void{
+    this.resultado = this.numero1 + this.numero2;    
+  }
+  restar():void{
+    this.resultado = this.numero1 - this.numero2;    
+  }
 }
