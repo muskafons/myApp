f96e2cf56f4a2fde814ecab6a8ce1e0c126506c0
docgestion.dtic <docgestion.dtic@uptc.edu.co>
2022-03-23 15:30:04 -0500

    registro de usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,39 @@ import { Empleado } from './empleado.modelo';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+cuadroNombre:string="";
+cuadroApellido:string="";
+cuadroCargo:string="";
+cuadroSalario:number=0;
+
+mensaje="Registro de Usuarios";
+
+titulo = 'REGISTRO DE USUARIOS';
+
   empleados: Empleado[] = [
     new Empleado('Julian', 'Fonseca', 'tecnico', 1000),
     new Empleado('Camilo', 'Fonseca', 'tecnico', 1500),
     new Empleado('Jaen', 'Rutts', 'auxiliar', 900),
     new Empleado('Marian', 'Reds', 'ingeniera', 1200),
   ];
-  titulo = 'REGISTRO DE USUARIOS';
+
+  addRegistro(){
+    if (this.cuadroNombre==""||this.cuadroCargo==""||this.cuadroApellido==""||this.cuadroSalario==0) {
+      this.mensaje = "POR FAVOR COMPLETE LOS CAMPOS";
+    }
+    else{
+      let empleado = new Empleado(this.cuadroNombre,this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+      this.empleados.push(empleado);
+      this.mensaje = "USUARIO REGISTRADO"
+      this.cuadroNombre = "";
+      this.cuadroApellido = "";
+      this.cuadroCargo = "";
+      this.cuadroSalario = 0;
+
+    }
+
+ 
+  }
+
 }
