de888ec1ffcf4ff96ef0108898f1df5e1f0f453f
docgestion.dtic <docgestion.dtic@uptc.edu.co>
2022-03-26 12:38:10 -0500

    Routing boton a componente

--- src/app/proyectos.component/proyectos.component.ts
+++ src/app/proyectos.component/proyectos.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServicioEmpleadoService } from '../servicio-empleado.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
-  styleUrls: ['./proyectos.component.css']
+  styleUrls: ['./proyectos.component.css'],
 })
 export class ProyectosComponent implements OnInit {
+  mensaje: string = 'WELCOME';
 
-  constructor() { }
+  constructor(private router: Router, private servicio:ServicioEmpleadoService) {}
 
-  ngOnInit(): void {
+  cuadroNombre: string = '';
+  cuadroApellido: string = '';
+  cuadroCargo: string = '';
+  cuadroSalario : number = 0;
+
+  volver() {
+    this.router.navigate(['']);
   }
 
+  useServicio(){
+    this.mensaje=this.servicio.addRegistro(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    this.cuadroNombre = "";
+    this.cuadroApellido = "";
+    this.cuadroCargo = "";
+    this.cuadroSalario = 0;
+    if(this.mensaje=="USUARIO REGISTRADO"){
+      this.router.navigate(['']);
+    }
+  }
+
+  ngOnInit(): void {}
 }
